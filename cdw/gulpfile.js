32d4ad2d5a4368d74ea050555edb85d570e0f468
ROHEUNCHAN <nong0324@naver.com>
2023-04-28 09:54:32 +0900

    fix

--- cdw/gulpfile.js
+++ cdw/gulpfile.js
@@ -1,26 +1,18 @@
-// 'use strict'
-const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
-const Fiber = require('fibers');
-const sourcemaps = require('gulp-sourcemaps'); 
+const gulp = require('gulp');
+const sass = require('gulp-dart-sass');
+const sourcemaps = require('gulp-sourcemaps');
+sass.compiler = require('sass');
 
-
-gulp.task('cdw', function(){
-  return gulp
-    .src("scss/*.scss") 
-    .pipe(sourcemaps.init()) 
-    .pipe(sass({fiber:Fiber}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps')) 
-    .pipe(gulp.dest("dist/css")) 
+gulp.task('cdw', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
 });
 
-// ★★★★ add start : 감시대상 scss가 변동되면 자동으로 업데이트!
 gulp.task('watch', function () {
-  gulp.watch('scss/**/*.scss', gulp.series('cdw')); // 감시해야할 scss 위치 지정
+  gulp.watch('./sass/**/*.scss', gulp.series('cdw'));
 });
-// ★★★★ add end
 
-gulp.task(
-    "default",
-    gulp.parallel("cdw", "watch") 
-);
+gulp.task('default', gulp.series('cdw', 'watch'));
